b86962747144f9e959428fc30547796491f3c6a9
AminovJamshid <aminovjamshid777@gmail.com>
2024-11-13 14:59:29 +0500

    MINOR: writing routes and resources

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// The root uri of application
+Route::get('/', 'TodoItemController@index')->name('showTodosPage');
+Route::get('/todos', 'TodoItemController@todos')->name('loadTodos');
+Route::post('/todos', 'TodoItemController@store')->name('storeTodos');
+Route::put('/todos/{todoItem}', 'TodoItemController@update')->name('updateTodo');
+Route::delete('/todos/{todoItem}', 'TodoItemController@destroy')->name('deleteTodo');
